ff2983f1da07344c5d4af0c747cd865c8b8d4224
atachimiko <atachi.miko@gmail.com>
2014-06-08 04:19:36 +0900

    Technology Preview

---
+++ KanColleOneDropTw/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Livet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TweetSharp;
+
+namespace KanColleOneDropTw
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		// TwitterAPIを利用するための各種キー
+		// 
+		// twitterConsumerKey    : APIサービスを使用するアプリケーション登録
+		// twitterConsumerSecret : Keyに対するパスワード
+		// AccessToken           : API利用者側が上記のカスタマーキーを使用する際に発行されるトークン
+		// AccessTokenSecret     : AccessTokenのパスワード
+
+		static string twitterConsumerKey = "******************";
+		static string twitterConsumerSecret = "************************";
+		static string AccessToken = "************************";
+		static string AccessTokenSecret = "*******************";
+		static TwitterService service;
+		static string ApplicationDirectoryPath;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			InitializeDirectory();
+
+			service = new TwitterService(twitterConsumerKey, twitterConsumerSecret);
+			service.AuthenticateWith(AccessToken, AccessTokenSecret);
+
+			DispatcherHelper.UIDispatcher = this.Dispatcher;
+
+			InitializeItems();
+		}
+
+		/// <summary>
+		///     保存先ディレクトリの作成と指定
+		/// </summary>
+		void InitializeDirectory()
+		{
+			// Windowsのマイドキュメント内
+			// 「KanColleOneDropTw」というフォルダを(なければ)作成します。
+			string personalDirectoryPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			ApplicationDirectoryPath = System.IO.Path.Combine(personalDirectoryPath, @"KanColleOneDropTw");
+			System.IO.Directory.CreateDirectory(ApplicationDirectoryPath);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		void InitializeItems()
+		{
+			DispatcherCollection<TweetListItemData> items = new DispatcherCollection<TweetListItemData>(DispatcherHelper.UIDispatcher);
+
+			SearchOptions searchOption = new SearchOptions();
+			searchOption.Q = "-RT #艦これ版深夜の真剣お絵描き60分一本勝負";
+			searchOption.Resulttype = TwitterSearchResultType.Recent;
+			searchOption.Count = 10;
+			searchOption.Lang = "ja";
+			//searchOption.MaxId = 0L;
+
+			IAsyncResult result = service.Search(searchOption, (searchResult, response) =>
+			{
+				if (response.StatusCode == HttpStatusCode.OK)
+				{
+					foreach (var tweet in searchResult.Statuses)
+					{
+						var i = new TweetListItemData(tweet);
+						items.Add(i);
+
+						// BackgroundWorkerを使えばいいのだけれど。
+						DispatcherHelper.UIDispatcher.Invoke(() =>
+						{
+							if (tweet.Entities.Count() > 0)
+							{
+								var photo = tweet.Entities.Media.FirstOrDefault();
+								if (photo != null)
+								{
+									SaveMediaFile(photo.MediaUrl, tweet.Id);
+									i.PhotoImage = new BitmapImage(new Uri(photo.MediaUrl));
+								}
+							}
+
+							i.AvaterImage = new BitmapImage(new Uri(tweet.User.ProfileImageUrl));
+						});
+					}
+				}
+			});
+			//result.AsyncWaitHandle.WaitOne();
+
+			this.TweetListView.ItemsSource = items;
+		}
+
+		/// <summary>
+		///     HTTPでダウンロードしたものをファイルとして保存(リクエストはWebClient依存)
+		/// </summary>
+		/// <param name="remoteMediaFileUrl"></param>
+		/// <param name="tweetId"></param>
+		void SaveMediaFile(string remoteMediaFileUrl, long tweetId)
+		{
+			var uri = new Uri(remoteMediaFileUrl);
+			var localFileName = tweetId + System.IO.Path.GetExtension(uri.LocalPath);
+
+			WebClient webClient = new WebClient();
+			webClient.DownloadFile(remoteMediaFileUrl, System.IO.Path.Combine(ApplicationDirectoryPath, localFileName));
+		}
+	}
+
+	public class TweetListItemData
+	{
+		public TweetListItemData(TwitterStatus model)
+		{
+			this.TwitterModel = model;
+		}
+
+		readonly TwitterStatus TwitterModel;
+
+		public string BDName { get { return string.Format("{0} @{1}", this.TwitterModel.User.Name, this.TwitterModel.User.ScreenName); } }
+		public DateTime CreateDate { get { return this.TwitterModel.CreatedDate; } }
+		public DateTime CreateDateJp { get { return this.TwitterModel.CreatedDate.AddHours(9); } }
+		public string TweetText { get { return this.TwitterModel.Text; } }
+
+		public BitmapImage PhotoImage { get; set; }
+		public BitmapImage AvaterImage { get; set; }
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 KanColleOneDropTw
 =================
+
+ハッシュタグ #艦これ版深夜の真剣お絵描き60分一本勝負 をTwitterAPIを使って検索し、
+取得したツイートから画像ファイルを表示・保存するサンプルアプリケーションです。
+
+このアプリケーションは **開発者向け** ですのでバイナリの配布は行っていません。
+
+TwitterAPIを使用するためのカスタマーキーなどは各自取得してください。
+クライアントのためのトークン生成も各自行ってください。
+
+## サポート ##
+このパッケージの利用の結果生じた損害について、一切責任を負いません。
+自己責任でお願いします。
+
+ソースコードのライセンスについてはLICENCEを参照してください。
+
+## 使い方 ##
+TwitterAPIを利用するためのカスタマーキーを発行します
+https://dev.twitter.com/
+
+次にクライアント用のトークンを発行します
+```c#
+string twitterConsumerKey = "***"; // カスタマーキー
+string twitterConsumerSecret = "***"; // カスタマーキーのパスワード
+ 
+var service = new TwitterService(twitterConsumerKey, twitterConsumerSecret);
+ 
+// Step 1 - Retrieve an OAuth Request Token
+OAuthRequestToken requestToken = service.GetRequestToken();
+ 
+// Step 2 - Redirect to the OAuth Authorization URL
+Uri uri = service.GetAuthorizationUri(requestToken);
+Process.Start(uri.ToString()); // ブラウザが起動し、認証コードが表示される(Twitterにログインしていない場合は、ログインページが先に表示される)
+ 
+// Step 3 - Exchange the Request Token for an Access Token
+string verifier = "123456"; // <-- ブラウザに表示された認証コードを入力
+OAuthAccessToken access = service.GetAccessToken(requestToken, verify);
+
+// access.Tokenがアクセストークンです。
+// access.TokenSecretがアクセストークンのパスワードです。
+```
+
+TwitterAPIを実際に使うには、「カスタマーキー、カスタマーキーのパスワード、アクセストークン、アクセストークンのパスワード」の4つのキー文字列が必要です。
+
